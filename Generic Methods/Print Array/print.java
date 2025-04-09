754135e67149a1636aaa0d0a9f00e08516c78ccf
Aviral Varshney <aviralvarshney07@gmail.com>
2025-04-09 11:42:06 +0530

    Printing array using Generic Methods

---
+++ Generic Methods/Print Array/print.java
@@ -0,0 +1,16 @@
+public class print {
+    public static <T> void printArray(T[] array) {
+        for (T item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3};
+        String[] strArray = {"A", "B", "C"};
+
+        printArray(intArray);  // prints: 1 2 3
+        printArray(strArray);  // prints: A B C
+    }
+}
